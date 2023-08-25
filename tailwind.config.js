6fb4ca353de3917164f96c29c9c565ae825bfefc
Aditya <132193415+ascendantaditya@users.noreply.github.com>
2023-08-25 23:48:47 +0530

    Patient Profile 3

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,23 +4,25 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        dimgray: {
-          "100": "#6a6a6a",
-          "200": "#646464",
+        white: "#fff",
+        cornflowerblue: {
+          "100": "#0272cc",
+          "200": "rgba(35, 138, 228, 0.41)",
         },
         black: "#000",
-        gainsboro: "rgba(217, 217, 217, 0)",
-        white: "#fff",
-        royalblue: "#0042ab",
+        dimgray: "#696969",
         gray: "rgba(255, 255, 255, 0.6)",
       },
       fontFamily: {
         roboto: "Roboto",
       },
+      borderRadius: {
+        "sm-1": "13.1px",
+      },
     },
     fontSize: {
-      "18xl": "37px",
-      "5xl": "24px",
+      "9xl": "28px",
+      "15xl-8": "34.8px",
       inherit: "inherit",
     },
   },
